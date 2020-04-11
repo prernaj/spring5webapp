b6ccdd80154b7187163c5924a35c92bca99b3112
Prerna Jeslani <prernajeslani@Prernas-MacBook-Pro.local>
2020-04-11 15:53:13 +0530

    Assignment: adding Publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository) {
+    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -24,14 +28,15 @@ public class BootStrapData implements CommandLineRunner {
         Book killingAnimals = new Book("Killing animals", "12321");
         sameera.getBooks().add(killingAnimals);
         killingAnimals.getAuthors().add(sameera);
+        Publisher publisher = new Publisher("Sanjay Dutt", "plot number 123", "kanpur", "UP", "209098");
 
         bookRepository.save(killingAnimals);
         authorRepository.save(sameera);
+        publisherRepository.save(publisher);
 
         System.out.println("Started in bootstrap");
         System.out.println("Number of books " + bookRepository.count());
-
-
+        System.out.println("Number of publishers " + publisherRepository.count());
 
     }
 }
